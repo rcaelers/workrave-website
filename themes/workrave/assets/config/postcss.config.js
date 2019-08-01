33c4584f51db80749cac77bcb5fcc5eb437d6356
Rob Caelers <rob.caelers@gmail.com>
2019-08-01 13:59:33 +0200

    Move browerconfig to separate file.
    Signed-off-by: Rob Caelers <rob.caelers@gmail.com>

--- themes/workrave/assets/config/postcss.config.js
+++ themes/workrave/assets/config/postcss.config.js
@@ -9,16 +9,6 @@ module.exports = {
         },
         'cssnano': { autoprefixer: true },
         'autoprefixer': {
-            browsers: [
-                "Android 2.3",
-                "Android >= 4",
-                "Chrome >= 20",
-                "Firefox >= 24",
-                "Explorer >= 8",
-                "iOS >= 6",
-                "Opera >= 12",
-                "Safari >= 6"
-            ]
         }
     },
 }
